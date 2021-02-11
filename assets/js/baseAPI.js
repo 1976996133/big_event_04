235743a50c290ea518a5034828a5203124b6bda5
shs <1976996133@qq.com>
2021-02-11 22:57:20 +0800

    主页页面以完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,5 +6,22 @@
 $.ajaxPrefilter(function (options) {
     // console.log(options);
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    options.url = 'http://ajax.frontend.itheima.net' + options.url;
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //强制清空token
+            localStorage.removeItem('token')
+            // 强制跳转页面
+            location.href = '/login.html'
+        }
+    }
 })
